59ab80c397c5c4f8cbd24b381b1164494cd9340b
McEnerney <mcenerney1@llnl.gov>
2017-05-10 08:42:41 -0700

    debug

--- acme_diags/parallel/run_diag.sh
+++ acme_diags/parallel/run_diag.sh
@@ -3,8 +3,7 @@
 #SBATCH -N 1
 #SBATCH -t 00:30:00
 #SBATCH -J plotset5
-#SBATCH -D $HOME/acme_slurm/logs/
 #SBATCH -o set5_driver.o%j
-
+#SBATCH -D $HOME/acme_slurm/logs/
 source activate 2.8
 $HOME/acme_slurm/ps5_commands.sh
